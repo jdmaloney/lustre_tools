800f292392a5be47add9500551c7f4ff663e2f18
malone12 <malone12@illinois.edu>
2021-05-21 07:28:56 -0500

    Log path to variable

--- exa_snap.sh
+++ exa_snap.sh
@@ -2,39 +2,40 @@
 
 filesystems=(testfs) ## File Systems to Snapshot
 retention=4 ## Days
+log_path="/var/log/exa_snap/snapshot.log"
 
 for f in ${filesystems[@]}
 do
 	existing_snaps=$(iml snapshot list ${f} | grep ${f} | wc -l)
-	echo "$(date +%Y%m%d_%H%M%S) ${existing_snaps} currently exist for ${f} (prior to run)" >> /var/log/exa_snap/snapshot.log
+	echo "$(date +%Y%m%d_%H%M%S) ${existing_snaps} currently exist for ${f} (prior to run)" >> "${log_path}"
 	if [ ${existing_snaps} -lt ${retention} ]; then
-		echo "$(date +%Y%m%d_%H%M%S) Creating snapshot for ${f}" >> /var/log/exa_snap/snapshot.log
+		echo "$(date +%Y%m%d_%H%M%S) Creating snapshot for ${f}" >> "${log_path}"
 		iml snapshot create ${f} ${f}_$(date +%Y%m%d_%H%M)
 		if [ $? -eq 0 ]; then
-			echo "$(date +%Y%m%d_%H%M%S) Snapshot created, exiting run" >> /var/log/exa_snap/snapshot.log
+			echo "$(date +%Y%m%d_%H%M%S) Snapshot created, exiting run" >> "${log_path}"
 		else
-			echo "$(date +%Y%m%d_%H%M%S) Error creating snapshot" >> /var/log/exa_snap/snapshot.log
+			echo "$(date +%Y%m%d_%H%M%S) Error creating snapshot" >> "${log_path}"
 		fi
 	else
-		echo "$(date +%Y%m%d_%H%M%S) Creating snapshot for ${f}" >> /var/log/exa_snap/snapshot.log
+		echo "$(date +%Y%m%d_%H%M%S) Creating snapshot for ${f}" >> "${log_path}"
 		iml snapshot create ${f} ${f}_$(date +%Y%m%d_%H%M)
 		if [ $? -eq 0 ]; then
-                        echo "$(date +%Y%m%d_%H%M%S) Snapshot created" >> /var/log/exa_snap/snapshot.log
+                        echo "$(date +%Y%m%d_%H%M%S) Snapshot created" >> "${log_path}"
                 else
-                        echo "$(date +%Y%m%d_%H%M%S) Error creating snapshot; aborting run" >> /var/log/exa_snap/snapshot.log
+                        echo "$(date +%Y%m%d_%H%M%S) Error creating snapshot; aborting run" >> "${log_path}"
 			exit 1
                 fi
 		to_remove=$((existing_snaps-retention+1))
-		echo "$(date +%Y%m%d_%H%M%S) Need to remove ${to_remove} snapshots from the system" >> /var/log/exa_snap/snapshot.log
+		echo "$(date +%Y%m%d_%H%M%S) Need to remove ${to_remove} snapshots from the system" >> "${log_path}"
 		remove_names=($(iml snapshot list ${f} | tail -n +4 | awk -v d=${f} '$2 == d {print $0}'| head -n ${to_remove} | awk '{print $4}' | xargs))
 		for r in ${remove_names[@]}
 		do
-			echo "$(date +%Y%m%d_%H%M%S) Removing snapshot ${r} for file system ${f}" >> /var/log/exa_snap/snapshot.log
+			echo "$(date +%Y%m%d_%H%M%S) Removing snapshot ${r} for file system ${f}" >> "${log_path}"
 			iml snapshot destroy ${f} ${r}
 			if [ $? -eq 0 ]; then
-               		       	echo "$(date +%Y%m%d_%H%M%S) Snapshot destroyed successfully" >> /var/log/exa_snap/snapshot.log
+               		       	echo "$(date +%Y%m%d_%H%M%S) Snapshot destroyed successfully" >> "${log_path}"
                 	else
-                        	echo "$(date +%Y%m%d_%H%M%S) Error creating snapshot; aborting run" >> /var/log/exa_snap/snapshot.log
+                        	echo "$(date +%Y%m%d_%H%M%S) Error creating snapshot; aborting run" >> "${log_path}"
 				exit 1
                 	fi
 		done	
