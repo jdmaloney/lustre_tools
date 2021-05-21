874f5ea363faad750b0ef806db7d3a428eefd2db
malone12 <malone12@illinois.edu>
2021-05-21 08:36:54 -0500

    Config file setup for exa_snap

--- exa_snap/exa_snap.sh
+++ exa_snap/exa_snap.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-filesystems=(testfs) ## File Systems to Snapshot
-retention=4 ## Days
-log_path="/var/log/exa_snap/snapshot.log"
+source /etc/exa_snap.conf
 
 for f in ${filesystems[@]}
 do
-	existing_snaps=$(iml snapshot list ${f} | grep ${f} | wc -l)
+	existing_snaps=$(/usr/bin/iml snapshot list ${f} | grep ${f} | wc -l)
 	echo "$(date +%Y%m%d_%H%M%S) ${existing_snaps} currently exist for ${f} (prior to run)" >> "${log_path}"
 	if [ ${existing_snaps} -lt ${retention} ]; then
 		echo "$(date +%Y%m%d_%H%M%S) Creating snapshot for ${f}" >> "${log_path}"
-		iml snapshot create ${f} ${f}_$(date +%Y%m%d_%H%M)
+		/usr/bin/iml snapshot create ${f} ${f}_$(date +%Y%m%d_%H%M)
 		if [ $? -eq 0 ]; then
 			echo "$(date +%Y%m%d_%H%M%S) Snapshot created, exiting run" >> "${log_path}"
 		else
@@ -18,7 +16,7 @@ do
 		fi
 	else
 		echo "$(date +%Y%m%d_%H%M%S) Creating snapshot for ${f}" >> "${log_path}"
-		iml snapshot create ${f} ${f}_$(date +%Y%m%d_%H%M)
+		/usr/bin/iml snapshot create ${f} ${f}_$(date +%Y%m%d_%H%M)
 		if [ $? -eq 0 ]; then
                         echo "$(date +%Y%m%d_%H%M%S) Snapshot created" >> "${log_path}"
                 else
@@ -27,11 +25,11 @@ do
                 fi
 		to_remove=$((existing_snaps-retention+1))
 		echo "$(date +%Y%m%d_%H%M%S) Need to remove ${to_remove} snapshots from the system" >> "${log_path}"
-		remove_names=($(iml snapshot list ${f} | tail -n +4 | awk -v d=${f} '$2 == d {print $0}'| head -n ${to_remove} | awk '{print $4}' | xargs))
+		remove_names=($(/usr/bin/iml snapshot list ${f} | tail -n +4 | awk -v d=${f} '$2 == d {print $0}'| head -n ${to_remove} | awk '{print $4}' | xargs))
 		for r in ${remove_names[@]}
 		do
 			echo "$(date +%Y%m%d_%H%M%S) Removing snapshot ${r} for file system ${f}" >> "${log_path}"
-			iml snapshot destroy ${f} ${r}
+			/usr/bin/iml snapshot destroy ${f} ${r}
 			if [ $? -eq 0 ]; then
                		       	echo "$(date +%Y%m%d_%H%M%S) Snapshot destroyed successfully" >> "${log_path}"
                 	else
